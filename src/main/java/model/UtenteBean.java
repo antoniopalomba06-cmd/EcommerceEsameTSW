c2228c4fcd2a0ef239da1f970cf244e66a2ce81f
Antonio <antonio.palomba06@gmail.com>
2026-06-03 17:00:29 +0200

    Creazione UtenteBean nel package model con attributi corrispondenti alDB

---
+++ src/main/java/model/UtenteBean.java
@@ -0,0 +1,70 @@
+package model;
+
+import java.io.Serializable;
+
+public class UtenteBean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    // Variabili che rispecchiano le colonne del database MySQL
+    private int id;
+    private String nome;
+    private String cognome;
+    private String email;
+    private String password;
+    private String ruolo;
+
+    // Costruttore vuoto obbligatorio per i Bean
+    public UtenteBean() {
+    }
+
+    // --- GETTER E SETTER ---
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRuolo() {
+        return ruolo;
+    }
+
+    public void setRuolo(String ruolo) {
+        this.ruolo = ruolo;
+    }
+}
