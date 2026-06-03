d7998470fe4ac04ae9970f6ff19e79af4e34edd3
Antonio <antonio.palomba06@gmail.com>
2026-06-03 17:21:49 +0200

    Creazione classe UtenteDAO
    Creata classe UtenteDAO con configurazione JNDI DataSource e metodo
    doSave per registrazione

---
+++ src/main/java/model/UtenteDAO.java
@@ -0,0 +1,58 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class UtenteDAO {
+
+    // Questa variabile conterrà il pool di connessioni
+    private static DataSource ds;
+
+    // Blocco statico che si avvia subito per pescare il file context.xml
+    static {
+        try {
+            Context initCtx = new InitialContext();
+            Context envCtx = (Context) initCtx.lookup("java:comp/env");
+            ds = (DataSource) envCtx.lookup("jdbc/EcommerceDB");
+        } catch (NamingException e) {
+            System.out.println("Errore di configurazione DataSource: " + e.getMessage());
+        }
+    }
+
+    // Metodo per salvare un nuovo utente nel Database
+    public synchronized void doSave(UtenteBean utente) throws SQLException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        // La query SQL con i punti interrogativi per evitare gli attacchi SQL Injection (il prof apprezzerà)
+        String insertSQL = "INSERT INTO Utente (nome, cognome, email, password, ruolo) VALUES (?, ?, ?, ?, ?)";
+
+        try {
+            // Peschiamo una connessione libera dal pool
+            connection = ds.getConnection();
+            preparedStatement = connection.prepareStatement(insertSQL);
+            
+            // Riempiamo i punti interrogativi coi dati del Bean
+            preparedStatement.setString(1, utente.getNome());
+            preparedStatement.setString(2, utente.getCognome());
+            preparedStatement.setString(3, utente.getEmail());
+            preparedStatement.setString(4, utente.getPassword());
+            preparedStatement.setString(5, utente.getRuolo());
+
+            // Eseguiamo il salvataggio
+            preparedStatement.executeUpdate();
+            
+        } finally {
+            try {
+                if (preparedStatement != null) preparedStatement.close();
+            } finally {
+                if (connection != null) connection.close(); // Questo non distrugge la connessione, la restituisce al pool
+            }
+        }
+    }
+}
